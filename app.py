f51383bb63c8b9b004955c882175284331aa1434
Adam Fields <me@aef.me>
2024-08-21 19:28:55 -0400

    Fix entrypoint

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-#!/usr/bin/env python
-import subprocess
-import sys
-
-try:
-    subprocess.run([sys.executable, "-m", "streamlit", "run", "0_🏠_Home.py"], check=True)
-except KeyboardInterrupt:
-    sys.exit(0)
-except subprocess.CalledProcessError as e:
-    sys.exit(e.returncode)
+#!/usr/bin/env -S streamlit run 0_🏠_Home.py
